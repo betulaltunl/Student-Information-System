7d15d0d3fdc657f56b5e424f5135750de2c55495
Betül Altunel <101793578+betulaltunl@users.noreply.github.com>
2022-12-05 14:21:42 +0300

    Add files via upload

---
+++ Teacher.java
@@ -0,0 +1,15 @@
+
+package javaapplication3;
+
+
+public class Teacher {
+    String name;
+    String mpno;//TELEFON NUMARASI
+    String branch;//BÖLÜMÜ
+
+    public Teacher(String name, String mpno, String branch) {
+        this.name = name;
+        this.mpno = mpno;
+        this.branch = branch;
+    }
+}
