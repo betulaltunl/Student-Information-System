b2b985e3d277635dc35c67d449484f3c39549d1b
Betül Altunel <101793578+betulaltunl@users.noreply.github.com>
2022-12-05 14:20:56 +0300

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,38 @@
+
+package javaapplication3;
+
+
+public class Course {
+    Teacher courseTeacher;
+    String name;
+    String code;
+    String prefix;
+    int note;
+    int verbalNote;
+
+    public Course(String name, String code, String prefix) {
+        this.name = name;
+        this.code = code;
+        this.prefix = prefix;
+        this.note = 0;
+        this.verbalNote = 0;
+        
+    }
+
+    public void addTeacher(Teacher t) {//KURSA ÖĞRETMEN EKLEME
+        if (this.prefix.equals(t.branch)) {
+            this.courseTeacher = t;
+            System.out.println("İşlem başarılı");
+        } else {
+            System.out.println(t.name + " Akademisyeni bu dersi veremez.");
+        }
+    }
+
+    public void printTeacher() {//KURSDA BULAN ÖĞRETMEN BİLGİSİNİ EKRANA YAZMA
+        if (courseTeacher != null) {
+            System.out.println(this.name + " dersinin Akademisyeni : " + courseTeacher.name);
+        } else {
+            System.out.println(this.name + " dersine Akademisyen atanmamıştır.");
+        }
+    }
+}
